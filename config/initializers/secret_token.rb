b9f146f4c7854f3a42d15b728427ccb21134a841
Rajesh Sharma <rajesh@surf.net.np>
2013-05-01 01:38:32 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BourkeApp::Application.config.secret_token = 'f2e98b43bc4aa6e6e1b44cb341a942d22ce6fd0d332549707ca4ac12bda9a18ab3febb3ffa4e6ecd6b199cdf402df734361a376fc7f40ebaae755598bc40fba4'
